a5a21387c42be1b5bf8213541edb2c7ee3ab237e
hartpableo <hart.hpwebdev@gmail.com>
2026-04-17 09:32:17 +0800

    Increased complexity of roll-the-dice script

--- dice-rolling-game/dice_rolling_game.py
+++ dice-rolling-game/dice_rolling_game.py
@@ -11,6 +11,18 @@
     
 import sys, random
 
+num_of_dice = 2
+
+# Set the number of dice
+while True:
+    set_num_of_dice = input("How many dice do you want to roll? ")
+    if set_num_of_dice.isdigit() == False or int(set_num_of_dice) <= 0:
+        print("That is not a valid number of dice. Try again!")
+    else:
+        num_of_dice = int(set_num_of_dice)
+        break
+
+# Roll the dice
 yes_values = ["y", "yes", "sure", "ok", "okay"]
 no_values = ["n", "no", "nope", "nah"]
 
@@ -20,6 +32,6 @@ while True:
         print("(" + str(random.randint(1, 6)) + ", " + str(random.randint(1, 6)) + ")")
     elif roll in no_values:
         print("Okay. Good bye!")
-        sys.exit()
+        break;
     else:
-        print("\"y\" or \"n\" values only acceptable")
+        print("That is not a valid answer.")
