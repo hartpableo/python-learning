060264e7f2a143a496f9653ff1af4233a9afdd7b
hartpableo <hart.hpwebdev@gmail.com>
2026-04-18 09:31:31 +0800

    Rock/Paper/Scissors game: Implement scoring

--- rock-paper-scissors-game/rock_paper_scissors.py
+++ rock-paper-scissors-game/rock_paper_scissors.py
@@ -1,13 +1,16 @@
 import random
 
+choices = ["ROCK", "PAPER", "SCISSORS"]
 winning_choices_map = {
     "ROCK": "SCISSORS",
     "PAPER": "ROCK",
     "SCISSORS": "PAPER"
 }
 
-while True:
-    choices = ["ROCK", "PAPER", "SCISSORS"]
+human_score = 0
+machine_score = 0
+    
+while True:    
     machine_choice = random.choice(choices)
     human_choice = input("Rock, paper, or scissors? (rock/paper/scissors/exit) ").upper().strip()
 
@@ -26,5 +29,10 @@ while True:
         print("It's a tie!")
     elif winning_choices_map.get(human_choice) == machine_choice:
         print("You win!")
+        human_score += 1
     else:
         print("You Lose!")
+        machine_score += 1
+        
+    print("Scores: You: " + str(human_score) + "  |  " + "Me: " + str(machine_score))
+    print("")
